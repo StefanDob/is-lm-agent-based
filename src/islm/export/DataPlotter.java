80f60bbf379ac389c57671cdeb0fbd522b9008f6
StefanDob <stefan.dobrea@stud.tu-darmstadt.de>
2025-09-12 22:47:09 +0200

    unmet demand now better plotted

--- src/islm/export/DataPlotter.java
+++ src/islm/export/DataPlotter.java
@@ -6,35 +6,57 @@ import tech.tablesaw.api.DoubleColumn;
 import org.knowm.xchart.Histogram;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
+
+import islm.ISLMBuilder;
+
 import org.knowm.xchart.BitmapEncoder;
 import org.knowm.xchart.BitmapEncoder.BitmapFormat;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import repast.simphony.engine.schedule.ScheduledMethod;
 
 public class DataPlotter {
 
-    @ScheduledMethod(start = 1, interval = 1, priority = 1000)
+    @ScheduledMethod(start = ISLMBuilder.TOTAL_SIMULATION_TICKS, interval = 1, priority = 1000)
     public void exportAtTheEnd() {
         String inputCsv = "output/islm_daily_output.csv";
         String outputImage = "output/unmet_demand_density.png";
 
-        // Load CSV using Tablesaw
         try {
             Table table = Table.read().csv(inputCsv);
-
-            // Extract the unmet demand column
             DoubleColumn unmetDemand = table.doubleColumn("UnmetDemandRatio");
-
-            // Convert to Java list
             List<Double> values = unmetDemand.asList();
 
-            // Create histogram data (probability density)
-            Histogram histogram = new Histogram(values, 30); // 30 bins
+            // Separate zeros and non-zeros
+            List<Double> zeros = new ArrayList<>();
+            List<Double> nonZeros = new ArrayList<>();
+            for (Double v : values) {
+                if (v == 0.0) {
+                    zeros.add(v);
+                } else {
+                    nonZeros.add(v);
+                }
+            }
+
+            // Create histogram for non-zero values (e.g., 29 bins)
+            Histogram nonZeroHist = new Histogram(nonZeros, 29);
+
+            // Manually add a bin for zeros at the beginning
+            List<Double> xData = new ArrayList<>();
+            List<Double> yData = new ArrayList<>();
+
+            // Add zero bin
+            xData.add(0.0);
+            yData.add((double) zeros.size() / values.size() / 1); // density
+
+            // Add non-zero bins
+            for (int i = 0; i < nonZeroHist.getxAxisData().size(); i++) {
+                xData.add(nonZeroHist.getxAxisData().get(i));
+                yData.add(nonZeroHist.getyAxisData().get(i));
+            }
 
             // Build chart
             XYChart chart = new XYChartBuilder()
@@ -46,12 +68,9 @@ public class DataPlotter {
                     .build();
 
             chart.getStyler().setXAxisDecimalPattern("0.00");
+            chart.addSeries("UnmetDemandRatio", xData, yData);
 
-            chart.addSeries("UnmetDemandRatio", histogram.getxAxisData(), histogram.getyAxisData());
-
-            // Export to PNG
             BitmapEncoder.saveBitmap(chart, outputImage, BitmapFormat.PNG);
-
             System.out.println("PDF plot written to " + outputImage);
 
         } catch (IOException e) {
